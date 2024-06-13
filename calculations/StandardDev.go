17fb3e59e9009f9cd118503d0e9c0715146e1304
cliffootieno <clifford242526@gmail.com>
2024-06-13 16:29:47 +0300

    overlooked calculation of varience

--- calculations/StandardDev.go
+++ calculations/StandardDev.go
@@ -1,15 +1,19 @@
 package calculations
 
+import (
+	"math"
+)
+
 func StandardDev(num []float64) float64 {
 	mean := Mean(num)
 	var result []float64
 	for _, v := range num {
-		result = append(result, float64(v)-mean)
+		result = append(result, v-mean)
 	}
 	var sum float64
 	for _, w := range result {
-		sum = w + w
+		sum += w * w
 	}
-	standardDev := sum / mean
-	return standardDev
+	variance := sum / float64(len(num))
+	return math.Sqrt(variance)
 }
