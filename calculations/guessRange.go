572f23b1d9fc851af345d82375fe2288c54cfeb9
cliffootieno <clifford242526@gmail.com>
2024-06-13 16:28:07 +0300

    removed reconvertion float64

--- calculations/guessRange.go
+++ calculations/guessRange.go
@@ -3,8 +3,8 @@ package calculations
 import "math"
 
 func GuessRange(num []float64, currentNum float64, nextNum float64) (int, int) {
-	mean := float64(Mean(num))
-	standardDev := float64(StandardDev(num))
+	mean := Mean(num)
+	standardDev := StandardDev(num)
 
 	rangeStart := math.Max(currentNum-standardDev, mean-standardDev)
 	rangeEnd := math.Min(nextNum+standardDev, mean+standardDev)
