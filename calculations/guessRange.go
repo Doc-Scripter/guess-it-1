1c4ebbe26b306b238332af1eef2bcacd549b44dd
cliffootieno <clifford242526@gmail.com>
2024-07-28 15:10:46 +0300

    feat :change logic to changes in reading from std input

--- calculations/guessRange.go
+++ calculations/guessRange.go
@@ -2,18 +2,13 @@ package calculations
 
 import "math"
 
-func GuessRange(num []float64, currentNum float64, nextNum float64) (int, int) {
+func GuessRange(num []float64, currentNum float64) (int, int) {
 	mean := Mean(num)
 	standardDev := StandardDev(num)
 
 	rangeStart := math.Max(currentNum-standardDev, mean-standardDev)
-	rangeEnd := math.Min(nextNum+standardDev, mean+standardDev)
+	rangeEnd := math.Min(currentNum+standardDev, mean+standardDev)
 
-	rangeStart = math.Max(rangeStart, currentNum)
-	rangeEnd = math.Min(rangeEnd, nextNum)
-
-	if rangeStart > rangeEnd {
-		rangeStart, rangeEnd = rangeEnd, rangeStart
-	}
+	
 	return int(math.Round(rangeStart)), int(math.Round(rangeEnd))
 }
