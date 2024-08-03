a76d21285f55fb2e2a2cf762d7f231b193e7f23b
cliffootieno <clifford242526@gmail.com>
2024-08-03 03:39:06 +0300

    feat : add capability to switch between skewness and normal method of guessing the range

--- calculations/guessRange.go
+++ calculations/guessRange.go
@@ -2,13 +2,27 @@ package calculations
 
 import "math"
 
-func GuessRange(num []float64, currentNum float64) (int, int) {
+func GuessRange(num []float64, currentNum float64, isSkewed bool) (int, int) {
 	mean := Mean(num)
 	standardDev := StandardDev(num)
+	skewness := Skewness(num)
 
-	rangeStart := math.Max(currentNum-standardDev, mean-standardDev)
-	rangeEnd := math.Min(currentNum+standardDev, mean+standardDev)
+	var rangeStart, rangeEnd float64
 
-	
+	if isSkewed {
+		// Adjust for skewed distribution
+		rangeStart = mean - 2*standardDev // Example adjustment
+		rangeEnd = mean + 2*standardDev   // Example adjustment
+	} else {
+		if math.Abs(skewness) < 0.5 { // Assuming normal distribution
+			rangeStart = math.Max(currentNum-standardDev, mean-standardDev)
+			rangeEnd = math.Min(currentNum+standardDev, mean+standardDev)
+		} else {
+			// Handle case if the skewness condition changes (normally shouldn't happen in this design)
+			rangeStart = mean - 2*standardDev
+			rangeEnd = mean + 2*standardDev
+		}
+	}
+
 	return int(math.Round(rangeStart)), int(math.Round(rangeEnd))
 }
